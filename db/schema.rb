d80d03a14a71364fef7f1ea5358f4453c6491319
draobfrus <aelfci5am@gmail.com>
2022-06-09 07:08:56 +0900

    Add: 検証

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_05_110041) do
+ActiveRecord::Schema.define(version: 2022_06_08_113222) do
 
   create_table "tasks", force: :cascade do |t|
-    t.string "name"
+    t.string "name", limit: 30, null: false
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
